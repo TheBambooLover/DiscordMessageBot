43183029827e3b18c94739255fa5d8ae75575fd4
Santiago Canovari <santiagocanovarigmail.com>
2024-02-10 13:20:18 -0300

    Implemented both functionalities

--- script.py
+++ script.py
@@ -2,6 +2,12 @@ import pyautogui
 import time
 import random
 from datetime import datetime
+import numpy as np
+import pyautogui
+import time
+
+# Wait for 5 seconds before starting to click (optional, adjust as needed)
+time.sleep(1)
 
 def send_message():
     pyautogui.click(x=168, y=298)
@@ -18,16 +24,35 @@ def send_message():
     start_time = time.time()
     this_time = random.randint(370,600)
     print(this_time)
+    go_to_thread()
     while time.time()-start_time < this_time:
-        click_on_screen()
-        time.sleep(0.1)
+        click_by_color()
+        time.sleep(1)
 
+def click_by_color():
+    target_color = "#248046"  # Example color in IR format
+    # Get current pixel colors
+    pixel_color_1 = pyautogui.screenshot().getpixel((416, 950))
+    pixel_color_2 = pyautogui.screenshot().getpixel((511, 957))
 
+    # Convert colors to strings in IR format for case-insensitive comparison
+    pixel_color_str_1 = "#" + "".join(["{:02X}".format(c) for c in pixel_color_1])
+    pixel_color_str_2 = "#" + "".join(["{:02X}".format(c) for c in pixel_color_2])
 
-def click_on_screen():
+    # Click on pixel 1 if it changed to the target color
+    if pixel_color_str_1.lower() == target_color.lower():
+        pyautogui.click(416, 950)
+        pyautogui.click(511, 1030)
+        print(f"Clicked on pixel ({416}, {950}) with color {pixel_color_str_1}")
+
+    # Click on pixel 2 if it changed to the target color
+    if pixel_color_str_2.lower() == target_color.lower():
+        pyautogui.click(511, 957)
+        pyautogui.click(511, 1030)
+        print(f"Clicked on pixel ({511}, {957}) with color {pixel_color_str_2}")
+
+def go_to_thread():
     pyautogui.click(x=187, y=403)
-    pyautogui.click(x=416, y=950)
-    time.sleep(15)
 
 def main():
     while True:
